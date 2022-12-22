d7aecf11c24e6c44f2e9fade8403557a00eee9e6
KIM-SOOMINI <soomin@e-flying.co>
2022-12-22 19:27:02 +0900

    기본 수학 2

--- BaekJoon/1929_sosu.js
+++ BaekJoon/1929_sosu.js
@@ -7,25 +7,29 @@ const rl = readline.createInterface({
 rl.on("line", (line) => {
     input = line.split(' ').map((element) => parseInt(element));
 }).on("close", () => {
-    let resArr = []
+    let resArr = new Set();
+
     for (let i = input[0] ; i <= input[1] ; i++) {
-        let count = 0;
-        
-        for (let j = 1 ; j <= i ; j++) {
-            if (i % j === 0) {
-                count++;
-            }
+        resArr.add(i);
+    }
 
-            if (count > 2) {
-                break;
-            }
-        }
+    resArr.delete(1)
 
-        if (count === 2) {
-            resArr.push(i);
+    let sqrt = Math.sqrt(input[1]);
+
+    for (let i = 2 ; i <= sqrt ; i++) {
+        for (let j = i + i ; j <= input[1] ; j += i) {
+            resArr.delete(j);
         }
     }
 
-    console.log(resArr.join('\n'));
+    let result = [];
+
+    for (let i of resArr.values()) {
+        result.push(i)
+    }
+
+    console.log(result.join('\n'));
+
     process.exit;
 });
