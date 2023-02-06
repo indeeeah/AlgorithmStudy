b07d94916c31eda5317a0a38531407b6e151ade0
KIM-SOOMINI <soomin@e-flying.co>
2023-02-06 19:43:36 +0900

    집합과 맵

---
+++ BaekJoon/1764/1764_newbie.js
@@ -0,0 +1,45 @@
+/**
+ * 김진영이 듣도 못한 사람의 명단과, 보도 못한 사람의 명단이 주어질 때, 듣도 보도 못한 사람의 명단을 구하는 프로그램을 작성하시오.
+ * 첫째 줄에 듣도 못한 사람의 수 N, 보도 못한 사람의 수 M이 주어진다. 이어서 둘째 줄부터 N개의 줄에 걸쳐 듣도 못한 사람의 이름과, N+2째 줄부터 보도 못한 사람의 이름이 순서대로 주어진다. 이름은 띄어쓰기 없이 알파벳 소문자로만 이루어지며, 그 길이는 20 이하이다. N, M은 500,000 이하의 자연수이다.
+ * 듣도 못한 사람의 명단에는 중복되는 이름이 없으며, 보도 못한 사람의 명단도 마찬가지이다.
+ * 듣보잡의 수와 그 명단을 사전순으로 출력한다.
+ * 
+ * 예제 입력 1
+ * 3 4
+ * ohhenrie
+ * charlie
+ * baesangwook
+ * obama
+ * baesangwook
+ * ohhenrie
+ * clinton
+ * 
+ * 예제 출력 1
+ * 2
+ * baesangwook
+ * ohhenrie
+ */
+
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = fs.readFileSync(filePath).toString().split('\n');
+
+let [N, M] = input.shift().split(' ').map((element) => parseInt(element));
+
+let nameSet = new Set();
+
+for (let i = 0 ; i < N ; i++) {
+    nameSet.add(input[i]);
+}
+
+let answer = [];
+for (let i = N; i < input.length ; i++) {
+    if (nameSet.has(input[i])) {
+        answer.push(input[i]);
+    }
+}
+
+answer.sort();
+
+console.log(answer.length);
+console.log(answer.join('\n'));
